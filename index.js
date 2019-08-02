696372bbe44a5d0434742c1e0caf9c9fd57ba133
abir <abirchandrabhushan@gmail.com>
2019-08-02 23:37:03 +0530

    Working generator

---
+++ index.js
@@ -0,0 +1,64 @@
+const CURR_DIR = process.cwd()
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+const CHOICES = fs.readdirSync(`${CURR_DIR}/templates`);
+
+const QUESTIONS = [{
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function(input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Name may only include letters, numbers, underscores and hashes';
+    }
+  }
+];
+
+
+inquirer.prompt(QUESTIONS)
+  .then((answers) => {
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];
+    const templatePath = `${__dirname}/templates/${projectChoice}`;
+
+    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+    createDirectoryContents(templatePath, projectName)
+  })
+  .catch((error) => {
+    console.log(error + '\nVisit: https://github.com/abircb/project-generator/issues')
+  });
+
+function createDirectoryContents(templatePath, newProjectPath) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, 'utf8');
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      // recursive call
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+}
+
+module.exports = {
+  createDirectoryContents: createDirectoryContents
+}
---
+++ success/web/router/api/tweets/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const get = require('./get')
+
+module.exports = {
+  // eslint-disable-next-line
+  get: get
+}

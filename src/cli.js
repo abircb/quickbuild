49dd6726ee667f6de98490a528e9a3f597281cdf
abir <abirchandrabhushan@gmail.com>
2019-08-19 12:47:09 +0530

    Added usage info

--- src/cli.js
+++ src/cli.js
@@ -1,42 +1,56 @@
 import {
   createProject
 } from './main'
+import {
+  usageInfo
+} from '../lib/usage';
 const arg = require('arg')
 const inquirer = require('inquirer')
 
-function parseArgumentsIntoOptions (rawArgs) {
-  const args = arg({
-    '--git': Boolean,
-    '--quickestbuild': Boolean,
-    '--install': Boolean,
-    '--verbose': Boolean,
-    '--mit': Boolean,
-    '--apache': Boolean,
-    '--bsd': Boolean,
-    '--unlicensed': Boolean,
-    '-g': '--git',
-    '-q': '--quickestbuild',
-    '-i': '--install',
-    '-v': '--verbose',
-    '-u': '--unlicensed'
-  }, {
-    argv: rawArgs.slice(2)
-  })
-  return {
-    skipPrompts: args['--quickestbuild'] || false,
-    verbose: args['--verbose'] || false,
-    git: args['--git'] || false,
-    license_MIT: args['--mit'] || false,
-    license_Apache: args['--apache'] || false,
-    license_BSD: args['--bsd'] || false,
-    unlicensed: args['--unlicensed'] || false,
-    template: args._[0],
-    runInstall: args['--install'] || false,
-    projectName: 'quickestbuild'
+
+function parseArgumentsIntoOptions(rawArgs) {
+  try {
+    const args = arg({
+      '--help': Boolean,
+      '--verbose': Boolean,
+      '--quickestbuild': Boolean,
+      '--name': String,
+      '--git': Boolean,
+      '--install': Boolean,
+      '--mit': Boolean,
+      '--apache': Boolean,
+      '--bsd': Boolean,
+      '--unlicensed': Boolean,
+      '-n': '--name',
+      '-g': '--git',
+      '-q': '--quickestbuild',
+      '-i': '--install',
+      '-v': '--verbose',
+      '-u': '--unlicensed'
+    }, {
+      argv: rawArgs.slice(2),
+      permissive: false
+    })
+    return {
+      help: args['--help'] || false,
+      verbose: args['--verbose'] || false,
+      skipPrompts: args['--quickestbuild'] || false,
+      projectName: args['--name'] || undefined,
+      git: args['--git'] || false,
+      runInstall: args['--install'] || false,
+      license_MIT: args['--mit'] || false,
+      license_Apache: args['--apache'] || false,
+      license_BSD: args['--bsd'] || false,
+      unlicensed: args['--unlicensed'] || false,
+      template: args._[0],
+    }
+  } catch (err) {
+    usageInfo(err.message)
+    process.exit(1)
   }
 }
 
-async function promptForMissingOptions (options) {
+async function promptForMissingOptions(options) {
   const defaultTemplate = 'quickestbuild'
   if (options.skipPrompts) {
     return {
@@ -46,15 +60,19 @@ async function promptForMissingOptions (options) {
     }
   }
 
-  const questions = [{
-    name: 'projectName',
-    type: 'input',
-    message: 'Project name:',
-    validate: function (input) {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true
-      else return 'Name may only include letters, numbers, underscores and hashes'
-    }
-  }]
+  const questions = []
+
+  if (!options.projectName) {
+    questions.push({
+      name: 'projectName',
+      type: 'input',
+      message: 'Project name:',
+      validate: function(input) {
+        if (/^([A-Za-z\-\_\d])+$/.test(input)) return true
+        else return 'Name may only include letters, numbers, underscores and hashes'
+      }
+    })
+  }
 
   if (!options.template) {
     questions.push({
@@ -90,14 +108,14 @@ async function promptForMissingOptions (options) {
     ...options,
     template: options.template || answers.template,
     git: options.git || answers.git,
-    projectName: answers.projectName,
+    projectName: options.projectName || answers.projectName,
     license: options.license || options.license_BSD || options.license_MIT || options.license_Apache || answers.license,
     unlicensed: options.unlicensed
   }
 }
 
-export async function cli (args) {
-  let options = parseArgumentsIntoOptions(args)
+export async function cli(args) {
+  let options = await parseArgumentsIntoOptions(args)
   options = await promptForMissingOptions(options)
   await createProject(options)
 }

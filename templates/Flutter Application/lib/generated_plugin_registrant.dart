8a7367f2ba53d1b08f417e7dd8b4330e9b3fef1d
abir <abirchandrabhushan@gmail.com>
2020-03-12 14:33:39 +0000

    update Flutter template

---
+++ templates/Flutter Application/lib/generated_plugin_registrant.dart
@@ -0,0 +1,14 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+//import 'package:shared_preferences_web/shared_preferences_web.dart';
+//
+//void registerPlugins(PluginRegistry registry) {
+//  SharedPreferencesPlugin.registerWith(
+//      registry.registrarFor(SharedPreferencesPlugin));
+//  registry.registerMessageHandler();
+//}
